5044d135d144c0b113c06f8ffa1d82d7f5645c56
kirimin <minkarakirimade@gmail.com>
2020-01-15 13:40:51 +0900

    init

---
+++ front/main.js
@@ -0,0 +1,171 @@
+(function () {
+    // create matter engine
+    function createEngine(parentNode) {
+        const Engine = Matter.Engine;
+        const World = Matter.World;
+        const Bodies = Matter.Bodies;
+        const MouseConstraint = Matter.MouseConstraint;
+
+        let engine = Engine.create(parentNode, {
+            render: {
+                options: {
+                    wireframes: false,
+                    background: 'green',
+                    height: 1020,
+                    width: 1980,
+                }
+            }
+        });
+
+        // Create ground and wall
+        let ground = Bodies.rectangle(0, 1080, 1920, 60, { isStatic: true });
+        World.add(engine.world, [ground]);
+
+        let mouseConstraint = MouseConstraint.create(engine);
+        World.add(engine.world, mouseConstraint);
+
+        Engine.run(engine);
+        return engine;
+    }
+
+    // split emoji string
+    function stringToArray(str) {
+        return str.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]|[^\uD800-\uDFFF]/g) || [];
+    }
+
+    function createTexture(sourceCanvas, bounds) {
+        let canvas = document.createElement('canvas');
+        canvas.width = bounds.max.x - bounds.min.x + 1;
+        canvas.height = bounds.max.y - bounds.min.y + 1;
+
+        canvas.getContext('2d').drawImage(sourceCanvas, bounds.min.x, bounds.min.y, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
+        return canvas.toDataURL();
+    }
+
+    function alphaToWhite(data8U) {
+        for (let i = 0; i < data8U.length; i += 4) {
+            if (data8U[i + 3] == 0) {
+                data8U[i] = 255;
+                data8U[i + 1] = 255;
+                data8U[i + 2] = 255;
+                data8U[i + 3] = 255;
+            }
+        }
+    }
+
+    function createWordInfo(word) {
+        let canvas = document.createElement('canvas');
+        canvas.width = 50 * word.length;
+        canvas.height = 50;
+        let context = canvas.getContext('2d');
+
+        // draw text
+        context.fillStyle = 'black';
+        context.font = '40px sans-serif';
+        context.fillText(word, 10, 40);
+
+        let source = cv.imread(canvas);
+        alphaToWhite(source.data);
+        let destC1 = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC1);
+        let destC4 = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4);
+
+        cv.cvtColor(source, destC1, cv.COLOR_RGBA2GRAY);
+        cv.threshold(destC1, destC4, 254, 255, cv.THRESH_BINARY);
+        cv.bitwise_not(destC4, destC4);
+
+        let contours = new cv.MatVector();
+        let hierarchy = new cv.Mat();
+        cv.findContours(destC4, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE, { x: 0, y: 0});
+        hierarchy.delete();
+        destC1.delete();
+        destC4.delete();
+        source.delete();
+
+        let points = [];
+        for (let i = 0; i < contours.size(); i++) {
+            let d = contours.get(i).data32S;
+            for (let j = 0; j < d.length; j++) {
+                points.push(d[j]);
+            }
+        }
+        contours.delete();
+
+        if (points.length < 3) {
+            return null;
+        }
+
+        let _points = new cv.Mat(1, points.length / 2, cv.CV_32SC2);
+        let d = _points.data32S;
+        for (let i = 0; i < points.length; i++) {
+            d[i] = points[i];
+        }
+        let hull = new cv.Mat();
+        cv.convexHull(_points, hull);
+        _points.delete();
+
+        let vert = [];
+        d = hull.data32S;
+        for (let i = 0; i < d.length; i += 2) {
+            vert.push({ x: d[i], y: d[i + 1]});
+        }
+        hull.delete();
+
+        const bounds = Matter.Bounds.create(vert);
+        const texture = createTexture(canvas, bounds);
+
+        return {
+            vert: vert,
+            texture: texture
+        };
+    }
+
+    function addToWorld(engine, word, x) {
+        createWordInfo(word);
+
+        const info = createWordInfo(word);
+        if (info == null) {
+            console.warn('Can not add "' + word  + '" to world');
+            return;
+        }
+
+        let wordBody = Matter.Bodies.fromVertices(x, 0, info.vert, {
+            render: {
+                sprite: {
+                    texture: info.texture
+                }
+            }
+        });
+
+        Matter.World.add(engine.world, wordBody);
+    }
+
+    let engine = createEngine(document.getElementById('world'));
+    document.getElementById('world').addEventListener('click', () => {
+        const initialBodiesLength = engine.world.bodies.length;
+        addToWorld(engine, 'きりみんちゃん', 500);
+        addToWorld(engine, 'みんみん', 800);
+        addToWorld(engine, '💮', 1000);
+        for(let i = 0; i < 10; i++) {
+            addToWorld(engine, '(⁎˃ᴗ˂⁎)', 410 + 1 * 50);
+        }
+    });
+
+    const userId = "kirimin";
+    fetch(`https://api.twitter.com/1.1/search/tweets.json?q=from%3Atwitterdev&result_type=mixed&count=2`, {
+        mode: "cors",
+        headers: {
+            authorization: "",
+            oauth_consumer_key: "",
+        },
+        oauth_signature_method:"HMAC-SHA1", 
+        // oauth_timestamp="generated-timestamp", 
+        oauth_token:"", 
+        oauth_version:"1.0"
+    })
+    .then(response => {
+        console.log(response.status); // => 200
+        return response.json().then(userInfo => {
+            console.log(userInfo); // => {...}
+        });
+    });
+})();
